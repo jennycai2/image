3c82a0f5e61845dbf5bd4d83089aa157018232f1
qcai2002 <qcai2002@gmail.com>
2015-07-27 17:19:31 -0700

    tf-idf

--- nlp.py
+++ nlp.py
@@ -30,17 +30,50 @@ def process(docu):
     return new_tokens
 #process(D1)
 
-def get_token_cnt(wd, docu):
-    cnt = 0
-    for i in range(len(docu)):
-        if wd == docu[i]:
-            cnt += 1
-    return cnt
+def compute_tf(docu):
+    n = len(docu)
+    dict1 = {}
+    for i in range(n):
+        if docu[i] not in dict1:
+            dict1[docu[i]] = 1.0/n
+        else:
+            dict1[docu[i]] = (dict1[docu[i]] * n + 1.0)/n
+    return dict1
+#print compute_tf(['data', 'scientists', 'process', 'data'])
 
-def get_token_vec(vocab, docu):
-    vec = [0]*len(vocab)
-    for i in range(len(vocab)):
-        vec[i] = get_token_cnt(vocab[i], docu)
+def build_idf(vocab, docum):
+    d = len(vocab)
+    n = len(docum)
+    token_and_idf = {}
+    for i in range(d):
+        cnt = 0
+        for j in range(n):
+            if vocab[i] in docum[j]:
+                cnt += 1
+        if cnt != 0:
+            idf = float(n) / cnt   # n is total number of documents
+        else:
+            idf = -1.0
+        token_and_idf[vocab[i]] = idf
+    return token_and_idf
+
+
+def compute_weight(ordered_vocab, idf, docu):
+    vec = [0.0]*len(ordered_vocab)
+
+    tf = compute_tf(docu)
+    print 'tf', tf
+
+    for i in range(len(docu)):
+        tf1 = 0.0
+        idf1 = 0.0
+        if docu[i] in tf:
+            tf1 = tf[docu[i]]
+        if docu[i] in idf:
+            idf1 = idf[docu[i]]
+        idx = ordered_vocab.index(docu[i])
+        vec[idx] = tf1 * idf1
+        #print docu[i], tf1, idf1
     return vec
 
 def build_matrix(data):
@@ -50,17 +83,22 @@ def build_matrix(data):
         docum.append(process(data[i]))
     print docum
     flat_list = [item for sublist in docum for item in sublist]
-    #print flat_list
+
     vocab = list(set(flat_list))
     print vocab, len(vocab)
 
-    d = len(vocab)
+    idf = build_idf(vocab, docum)
+    print 'idf', idf
 
     matr = []
+    ordered_vocab = list(idf)
     for i in range(n):
-        vec = get_token_vec(vocab, docum[i])
+        vec = compute_weight(ordered_vocab, idf, docum[i])
         matr.append(vec)
-    print matr
+    print docum
+    print matr[0]
+    print matr[1]
+    print matr[2]
     return matr
 
 
