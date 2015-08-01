f199daab7028fee11e12c64880593d45927527f5
qcai2002 <qcai2002@gmail.com>
2015-08-01 16:48:07 -0700

    3-way-merge sort

--- sort.py
+++ sort.py
@@ -19,6 +19,7 @@ def num_of_inversions(arr1):
             cnt += 1
     return cnt
 
+
 def merge(arr1, arr2):
     print 'arr1, arr2', arr1, arr2
     n = len(arr1) + len(arr2)
@@ -27,7 +28,7 @@ def merge(arr1, arr2):
     i = 0
     j = 0
     for k in range(n-1):
-        if arr1[i] < arr2[j]:
+        if arr1[i] <= arr2[j]:
             arr0.append(arr1[i])
             i += 1
             if i== len(arr1):
@@ -56,4 +57,30 @@ def sort(arr1):
     arr2 = sort(arr2)
     return merge(arr0, arr2)
 
-print sort(arr7)
+#print sort(arr7)
+
+def threeWayMerge(arr1, arr2, arr3):
+    print 'arr1, arr2, arr3', arr1, arr2, arr3
+
+    arr12 = merge(arr1, arr2)
+    return merge(arr12, arr3)
+
+def threeWaySort(arr1):
+    #print 'arr1', arr1
+    n = len(arr1)
+    if n<3:
+        if n<2:
+            return arr1
+        else:
+            if arr1[0] <= arr1[1]:
+                return arr1
+            else:
+                return [arr1[1], arr1[0]]
+    arr0 = arr1[0:n/3]
+    arr2 = arr1[n/3:n*2/3]
+    arr4 = arr1[n*2/3:]
+    arr0 = threeWaySort(arr0)
+    arr2 = threeWaySort(arr2)
+    arr4 = threeWaySort(arr4)
+    return threeWayMerge(arr0, arr2, arr4)
+print threeWaySort(arr7)
