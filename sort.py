f05525443b4266db43f15102fe832646dc159ba3
qcai2002 <qcai2002@gmail.com>
2015-08-08 18:26:38 -0700

    quick sort pick first elememnt

--- sort.py
+++ sort.py
@@ -8,26 +8,40 @@ arr4 = [1,3, 2, 4]
 arr5 = [2, 4, 1, 3, 9]
 arr6 = [1, 3, 5, 2, 4, 6]
 arr7 = [2,5, 7, 9, 1, 8, 9]
+arrx = [2,5, 7, 9, 1, 8, 9, 1, 3, 5, 2, 4, 6]
 global_cnt = 0
 
 
-def quicksort(arr0):
-    n = len(arr0)
-    if n<=1:
+def exchange_pos(arr0, i, p_indx):
+    if (i-p_indx) == 1 or (i-p_indx) == -1:
+        temp = arr0[p_indx]
+        arr0[p_indx] = arr0[i]
+        arr0[i] = temp
         return arr0
-    arr1=[]
-    arr2=[]
-    p = arr0[0]
-    for i in range(1, n):
+    temp = arr0[p_indx]
+    arr0[p_indx] = arr0[i]
+    arr0[i] = arr0[p_indx + 1]
+    arr0[p_indx + 1] = temp
+    return arr0
+
+def quicksort(arr0, l, r):
+    print 'arr0, l, r', arr0, l, r
+    n = r-l
+    if n==0:
+        return []
+    if n==1:
+        return [arr0[l]]
+    p = arr0[l]
+    p_indx = l
+    for i in range(l+1, r):
         if arr0[i] < p:
-           arr1.append(arr0[i])
-        else:
-           arr2.append(arr0[i])
-    #print 'arr1, arr2', arr1, arr2
-    return quicksort(arr1) + [p] + quicksort(arr2)
+            arr0 = exchange_pos(arr0, i, p_indx)
+            p_indx += 1
+    #print 'arr0, p_indx', arr0, p_indx
+    return quicksort(arr0, l, p_indx) + [p] + quicksort(arr0, p_indx+1, r)
 
 
-q = quicksort(arr7)
+q = quicksort(arrx, 0, len(arrx))
 print q
 
 def merge(arr1, arr2):
@@ -96,7 +110,7 @@ def threeWaySort(arr1):
 
 #print threeWaySort(arr7)
 def merge_and_countSplitInv(arr1, arr2):
-    print 'arr1, arr2', arr1, arr2
+    #print 'arr1, arr2', arr1, arr2
     n = len(arr1) + len(arr2)
     arr0 = []
     cnt = 0
@@ -115,7 +129,7 @@ def merge_and_countSplitInv(arr1, arr2):
             arr0.append(arr2[j])
             j += 1
             cnt += (len(arr1) - i)
-            print 'cnt', cnt
+            #print 'cnt', cnt
             if j== len(arr2):
                 #print "wow, j is at max"
                 arr0.extend(arr1[i:])
@@ -138,7 +152,7 @@ def sort_and_count(arr1):
     #global_cnt += cnt
     return merge_and_countSplitInv(arr0, arr2)
 
-print 'arr', arr2
+#print 'arr', arr2
 cnt, arr0 = sort_and_count(arr2)
 #cnt, arr0 = sort_and_count(arr4)
 #cnt, arr0 = sort_and_count(arr6)
