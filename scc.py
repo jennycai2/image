061560115a33f12f217cbab8652b107de63edffe
qcai2002 <qcai2002@gmail.com>
2015-08-26 17:46:56 -0700

    code before using dict to store graph

--- scc.py
+++ scc.py
@@ -25,7 +25,38 @@ directed_list = [
 [5,0]
 ]
 
-def file_to_array(filename):
+directed_graph = {
+0: [],
+1: [2,3],
+2: [4],
+3: [5],
+4: [5,0],
+5: [0]
+}
+
+directed_edges = [
+[2, 1],
+[1, 3],
+[3, 2],
+[3, 4],
+[4,6],
+[6,7],
+[7,5],
+[5,6]
+]
+
+def edges_to_nodes(list1):
+    result = {}
+    # find out how many nodes
+    for i in range(len(list1)):
+        if list1[i][0] in result:
+            result[list1[i][0]] += [list1[i][1]]
+        else:
+            result[list1[i][0]] = [list1[i][1]]
+    print len(result)
+    return result
+
+def file_to_list(filename):
     text_file = open(filename, "r")
     arr = text_file.readlines()
     text_file.close()
@@ -40,13 +71,16 @@ def file_to_array(filename):
         for j in range(len(tok)-1): # the last one is '\n'
             vec.append(int ( tok[j] ) )
         arry.append(vec)
-    #print len(arry)
-    #print arry[len(arry)-1]
-    return arry
+    print len(arry)
+    print arry[len(arry)-1]
 
-#edges = file_to_array(file1)
-#edges = edges[0:10]
-#print edges
+    result = edges_to_nodes(arry)
+    if 875714 in result:
+        print result[875714]
+    return result
+
+#nodes = file_to_list(file1)
+
 
 def DFS(list1, node0, result, lb):
     print '\n', node0, lb
@@ -85,12 +119,42 @@ def topological_sort(list1):
 
     return result
 
-print topological_sort(directed_list)
+print len(directed_graph)
+print topological_sort(directed_graph)
 
+def DFS_edges(list1, node0, result, lb):
+    print '\n', node0, lb
+    print result
+    result[node0][0] = True # mark as visited
+    sublist = list1[node0] # assume list starts from indx 0
+    print sublist, len(sublist)
+    n = len(sublist)
+    #if (len(sublist)==1):
 
-def scc(list1):
+    for i in range(1, n):
+        node1 = sublist[i]
+        print i, 'node1', node1, len(sublist), n
+        #if result[5][0]==False:
+        if result[node1][0] == False:
+            print len(result), 'xxx'
+            result, lb = DFS_edges(list1, node1, result, lb)
 
-    return 1
+    result[node0][1] = lb
+    lb -= 1
+    return result, lb
+
+def scc(list1, s, t):
+    result = {}
+    # find out how many nodes
+    for i in range(len(list1)):
+        if list1[i][0] in result:
+            result[list1[i][0]] += [list1[i][1]]
+        else:
+            result[list1[i][0]] = [list1[i][1]]
+
+    return result
+
+print scc(directed_edges, 1, 7) # pass smallest node, biggest node
 
 # pute shortest path between s and t. We choose s as the root node
 def BreadthFirstSearch(list1, s, t):
@@ -117,4 +181,4 @@ def BreadthFirstSearch(list1, s, t):
         return result1[t][1] - result1[s][1]
     return -1
 
-print BreadthFirstSearch(undirected_list, 1, 5)
+#print BreadthFirstSearch(undirected_list, 1, 5)
