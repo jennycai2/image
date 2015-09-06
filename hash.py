7315c178c38ef5a7259d6a11b1cd5b45b4c0aea3
qcai2002 <qcai2002@gmail.com>
2015-09-05 20:20:20 -0700

    with hashtable

--- hash.py
+++ hash.py
@@ -10,7 +10,6 @@ class ListNode(object):
         self.val = x
         self.next = next
 
-
 class hashTable(object):
     def __init__(self, n):
         self.size = n
@@ -20,20 +19,40 @@ class hashTable(object):
         return val % self.size
 
     def insert(self, val):
-        new_node = ListNode(val)
         key = self.hash_function(val)
         if self.list1[key] == None:
-            self.list1[key] = new_node
+            self.list1[key] = ListNode(val)
         else:
             node = self.list1[key]
             while (node.next):
+                if node.val == val:
+                    return True  # we don't insert duplicate value
                 node = node.next
-            node.next = new_node
+            node.next = ListNode(val) # no value is same as it, insert it
         return True
 
     def delete(self, val):
         # if it is the last one
-        return True
+        key = self.hash_function(val)
+        if self.list1[key] == None:
+            return False
+
+        node = self.list1[key]
+        if node.val == val:
+            if node.next:
+                # need to remove the head node
+                self.list1[key] = node.next
+            else:
+                # it's the only node
+                self.list1[key] = None
+            return True
+        while (node.next):
+            if (node.next.val == val):
+                node.next = node.next.next
+                return True
+            node = node.next
+
+        return False
 
     def lookup(self, val):
         key = self.hash_function(val)
@@ -45,6 +64,13 @@ class hashTable(object):
                 return True
             node = node.next
         return False
+
+    def lookup_range(self, num1, num2):
+        cnt = 0
+        for i in range(num1, num2+1):
+            if self.lookup(i):
+                cnt += 1
+        return cnt
 
 class Solution1(object):
     def number_exists(self, num1, num2, list2):
@@ -65,8 +91,10 @@ class Solution1(object):
         for i in range(len(list1)):
             num1 = sum_range[0] - list1[i]
             num2 = sum_range[1] - list1[i]
-            cnt += self.number_exists(num1, num2, list1[i+1:])
-        return cnt
+            cnt += htable.lookup_range(num1, num2)
+            if list1[i] <= num2 and list1[i] >= num1:
+                cnt -= 1  # we should count self twice
+        return cnt/2  # since each pair is counted twice
 
 input1 = [1,2,3,4,9,3,2,1]
 sum_range = [5,6] # find all pairs whose sum is in this range (inclusive)
